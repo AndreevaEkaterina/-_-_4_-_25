c52936d47b9fd8b34e272f0f227dd7a46b41a8ca
Ekaterina <114247065+AndreevaEkaterina@users.noreply.github.com>
2022-10-15 16:55:25 +0800

    Решила задачу 25

--- Program.cs
+++ Program.cs
@@ -4,4 +4,13 @@
 // самостоятельно!
 // 3, 5 -> 243 (3⁵)
 // 2, 4 -> 16
-Console.WriteLine("Hello, World!");
+void Power(int a, int b)
+{
+    Console.Write($"{Math.Pow(a, b)}");
+}
+Console.Clear();
+Console.Write("Введите число: ");
+int A = int.Parse(Console.ReadLine()!);
+Console.Write("Введите число степени: ");
+int B = int.Parse(Console.ReadLine()!);
+Power(A, B);
